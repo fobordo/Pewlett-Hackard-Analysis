3982afaf4d1aa8fa2fc2b0728fc9b7f43aec04d7
Francesca Obordo <fobordo@gmail.com>
2021-12-01 09:56:00 -0500

    Updated schema.sql.

--- schema.sql
+++ schema.sql
@@ -40,8 +40,8 @@ CREATE TABLE dept_emp (
 	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (dept_no, emp_no)
 );
 
